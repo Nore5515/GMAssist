e6bf35eb913e72dbecd54699bc654e58078f86e4
Noah <Nore5515@gmail.com>
2017-03-21 22:50:56 -0500

    Added random weather

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,9 @@ public class Main {
         r.AddHoles();
         System.out.println("\n" + r.ToString());
 
+        Weather w = new Weather();
+        System.out.println(w.ToString());
+
         /*String[] sList = r.AdjacentTiles(1,1);
         System.out.println("\n\n");
         for (int x = 0; x < 9; x++) {
@@ -224,6 +227,312 @@ class Room{
     }
     
     
+}
+
+//ORDER OF CHECKING MATTERS
+//PRECIP
+//HUMID
+//TEMP
+//WIND
+//etc
+
+class Weather{
+
+    String temp;
+    String light;
+    String humidity;
+    String precip;
+    String wind;
+    String pressure;
+    int tempLvl,lightLvl,humidityLvl,precipLvl,windLvl,pressureLvl;
+
+    public Weather(){
+
+        Random rand = new Random();
+        int tempchance = rand.nextInt(100)+1;
+        int lightchance = rand.nextInt(100)+1;
+        int humiditychance = rand.nextInt(100)+1;
+        int precipchance = rand.nextInt(100)+1;
+        int windchance = rand.nextInt(100)+1;
+        int pressurechance = rand.nextInt(100)+1;
+
+        if (precipchance <= 30){
+            precipLvl = 0;
+        }
+        else if (30 < precipchance && precipchance <= 50){
+            precipLvl = 1;
+        }
+        else if (50 < precipchance && precipchance <= 70){
+            precipLvl = 2;
+        }
+        else if (70 < precipchance && precipchance <= 90){
+            precipLvl = 3;
+        }
+        else if (90 < precipchance && precipchance <= 100){
+            precipLvl = 4;
+        }
+        else{
+            precipLvl = 99999;
+        }
+
+        if (humiditychance <= 5){
+            humidityLvl = 0;
+        }
+        else if (5 < humiditychance && humiditychance <= 25){
+            humidityLvl = 1;
+        }
+        else if (25 < humiditychance && humiditychance <= 75){
+            humidityLvl = 2;
+        }
+        else if (75 < humiditychance && humiditychance <= 95){
+            humidityLvl = 3;
+        }
+        else if (95 < humiditychance && humiditychance <= 100){
+            humidityLvl = 4;
+        }
+        else{
+            humidityLvl = 99999;
+        }
+
+        if (tempchance <= 5){
+            tempLvl = 0;
+        }
+        else if (5 < tempchance && tempchance <= 25){
+            tempLvl = 1;
+        }
+        else if (25 < tempchance && tempchance <= 75){
+            tempLvl = 2;
+        }
+        else if (75 < tempchance && tempchance <= 95){
+            tempLvl = 3;
+        }
+        else if (95 < tempchance && tempchance <= 100){
+            tempLvl = 4;
+        }
+        else{
+            tempLvl = 99999;
+        }
+
+        if (windchance <= 5){
+            windLvl = 0;
+        }
+        else if (5 < windchance && windchance <= 45){
+            windLvl = 1;
+        }
+        else if (45 < windchance && windchance <= 70){
+            windLvl = 2;
+        }
+        else if (70 < windchance && windchance <= 95){
+            windLvl = 3;
+        }
+        else if (95 < windchance && windchance <= 100){
+            windLvl = 4;
+        }
+        else{
+            windLvl = 99999;
+        }
+
+        if (pressurechance <= 5){
+            pressureLvl = 0;
+        }
+        else if (5 < pressurechance && pressurechance <= 25){
+            pressureLvl = 1;
+        }
+        else if (25 < pressurechance && pressurechance <= 75){
+            pressureLvl = 2;
+        }
+        else if (75 < pressurechance && pressurechance <= 95){
+            pressureLvl = 3;
+        }
+        else if (95 < pressurechance && pressurechance <= 100){
+            pressureLvl = 4;
+        }
+        else{
+            pressureLvl = 99999;
+        }
+
+        if (lightchance <= 5){
+            lightLvl = 0;
+        }
+        else if (5 < lightchance && lightchance <= 25){
+            lightLvl = 1;
+        }
+        else if (25 < lightchance && lightchance <= 75){
+            lightLvl = 2;
+        }
+        else if (75 < lightchance && lightchance <= 95){
+            lightLvl = 3;
+        }
+        else if (95 < lightchance && lightchance <= 100){
+            lightLvl = 4;
+        }
+        else{
+            lightLvl = 99999;
+        }
+
+
+        //LIGHT COVERAGE
+        if (lightLvl < 0){
+            light = "Solar Eclipse (2x Perception--)";
+        }
+        else if (lightLvl == 0){
+            light = "Dark (Perception--)";
+        }
+        else if (lightLvl == 1){
+            light = "Dim (Perception--)";
+        }
+        else if (lightLvl == 2){
+            light = "Average (No effect)";
+        }
+        else if (lightLvl == 3){
+            light = "Bright (Perception++)";
+        }
+        else if (lightLvl == 4){
+            light = "Blinding (Perception--)";
+        }
+        else{
+            light = "Solar Flare (2x Perception--)";
+        }
+
+
+        //PRECIPTATION LEVEL
+        if (precipLvl < 0){
+            precip = "Drought (Less Humid)";
+            humidityLvl--;
+        }
+        else if (precipLvl == 0){
+            precip = "Clear (Perception++, Less Humid)";
+            humidityLvl--;
+        }
+        else if (precipLvl == 1){
+            precip = "Foggy (Stealth++)";
+        }
+        else if (precipLvl == 2){
+            precip = "Drizzle (No effect)";
+        }
+        else if (precipLvl == 3){
+            precip = "Rain (DEX--, END++)";
+        }
+        else if (precipLvl == 4){
+            precip = "Storm (DX--, More Humid)";
+            humidityLvl++;
+        }
+        else{
+            precip = "Hurricane (More Wind, Flooding, More Humid)";
+            humidityLvl++;
+        }
+
+
+        //HUMIDITY LEVEL
+        if (humidityLvl < 0){
+            humidity = "Wasteland (No leaving area)";
+        }
+        else if (humidityLvl == 0){
+            humidity = "Dust (STR--, need Water";
+        }
+        else if (humidityLvl == 1){
+            humidity = "Dry (STR-- without Water";
+        }
+        else if (humidityLvl == 2){
+            humidity = "Average (No effect)";
+        }
+        else if (humidityLvl == 3){
+            humidity = "Moist (INT-- in H. Armor)";
+        }
+        else if (humidityLvl == 4){
+            humidity = "Damp (INT--, Hotter)";
+            tempLvl++;
+        }
+        else{
+            humidity = "Dank (2x INT--)";
+        }
+
+
+        //TEMPERATURE LEVEL
+        if (tempLvl < 0){
+            temp = "Ice Age (2x END--, Mild DMG)";
+        }
+        else if (tempLvl == 0){
+            temp = "Freezing (END--, Need Heat)";
+        }
+        else if (tempLvl == 1){
+            temp = "Cool (END-- without Heat)";
+        }
+        else if (tempLvl == 2){
+            temp = "Warm (No effect)";
+        }
+        else if (tempLvl == 3){
+            temp = "Hot (END--)";
+        }
+        else if (tempLvl == 4){
+            temp = "Burning (Minor DMG)";
+        }
+        else{
+            temp = "Firestorm (Minor DMG per turn)";
+        }
+
+
+        //WIND LEVEL
+        if (windLvl < 0){
+            wind = "Silence (2x Stealth--)";
+        }
+        else if (windLvl == 0){
+            wind = "Absent (DEX++, Ranged++)";
+        }
+        else if (windLvl == 1){
+            wind = "Breezy (END++)";
+        }
+        else if (windLvl == 2){
+            wind = "Average (No effect)";
+        }
+        else if (windLvl == 3){
+            wind = "Piercing (Ranged--)";
+        }
+        else if (windLvl == 4){
+            wind = "Dangerous (Ranged--, Melee--)";
+        }
+        else{
+            wind = "Tornado (Major DMG Outdoors)";
+        }
+
+
+        //PRESSURE LEVEL
+        if (pressureLvl < 0){
+            pressure = "Weightless (2x DEX++, 2x STR++)";
+        }
+        else if (pressureLvl == 0){
+            pressure = "Low (INT++, Melee++)";
+        }
+        else if (pressureLvl == 1){
+            pressure = "Minor (Melee++)";
+        }
+        else if (pressureLvl == 2){
+            pressure = "Mild (No effect)";
+        }
+        else if (pressureLvl == 3){
+            pressure = "Considerable (Melee--)";
+        }
+        else if (pressureLvl == 4){
+            pressure = "Disorientating (Melee--, Will--)";
+        }
+        else{
+            pressure = "Crushing (2x All--)";
+        }
+
+
+    }
+
+    public String ToString(){
+        String s = new String();
+        s += "Temperature: \t\t" + temp;
+        s += "\nLight Coverage: \t" + light;
+        s += "\nHumidity:  \t\t\t" + humidity;
+        s += "\nPrecipitation: \t\t" + precip;
+        s += "\nWind: \t\t\t\t" + wind;
+        s += "\nAir Pressure: \t\t" + pressure;
+        return s;
+    }
+
 }
 
 class Crate{
